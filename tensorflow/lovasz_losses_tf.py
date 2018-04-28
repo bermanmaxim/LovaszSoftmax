1fe212b574af854a82a2265aec3a4ff76b4a5d86
Maxim Berman <bermanmaxim@gmail.com>
2018-04-28 16:01:54 +0200

    lovasz_losses_tf python 3 compat, class filter

--- tensorflow/lovasz_losses_tf.py
+++ tensorflow/lovasz_losses_tf.py
@@ -34,7 +34,8 @@ def lovasz_hinge(logits, labels, per_image=True, ignore=None):
       ignore: void class id
     """
     if per_image:
-        def treat_image((log, lab)):
+        def treat_image(log_lab):
+            log, lab = log_lab
             log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
             log, lab = flatten_binary_scores(log, lab, ignore)
             return lovasz_hinge_flat(log, lab)
@@ -91,41 +92,43 @@ def flatten_binary_scores(scores, labels, ignore=None):
 # --------------------------- MULTICLASS LOSSES ---------------------------
 
 
-def lovasz_softmax(probas, labels, only_present=False, per_image=False, ignore=None, order='BHWC'):
+def lovasz_softmax(probas, labels, classes='all', per_image=False, ignore=None, order='BHWC'):
     """
     Multi-class Lovasz-Softmax loss
       probas: [B, H, W, C] or [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1)
       labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
-      only_present: average only on classes present in ground truth
+      classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
       per_image: compute the loss per image instead of per batch
       ignore: void class labels
       order: use BHWC or BCHW
     """
     if per_image:
-        def treat_image((prob, lab)):
+        def treat_image(prob_lab):
+            prob, lab = prob_lab
             prob, lab = tf.expand_dims(prob, 0), tf.expand_dims(lab, 0)
             prob, lab = flatten_probas(prob, lab, ignore, order)
-            return lovasz_softmax_flat(prob, lab, only_present=only_present)
+            return lovasz_softmax_flat(prob, lab, classes=classes)
         losses = tf.map_fn(treat_image, (probas, labels), dtype=tf.float32)
         loss = tf.reduce_mean(losses)
     else:
-        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore, order), only_present=only_present)
+        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore, order), classes=classes)
     return loss
 
 
-def lovasz_softmax_flat(probas, labels, only_present=False):
+def lovasz_softmax_flat(probas, labels, classes='all'):
     """
     Multi-class Lovasz-Softmax loss
       probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
       labels: [P] Tensor, ground truth labels (between 0 and C - 1)
-      only_present: average only on classes present in ground truth
+      classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
     """
     C = probas.shape[1]
     losses = []
     present = []
-    for c in range(C):
-        fg = tf.cast(tf.equal(labels, c), probas.dtype) # foreground for class c
-        if only_present:
+    class_to_sum = list(range(C)) if classes in ['all', 'present'] else classes
+    for c in class_to_sum:
+        fg = tf.cast(tf.equal(labels, c), probas.dtype)  # foreground for class c
+        if classes == 'present':
             present.append(tf.reduce_sum(fg) > 0)
         errors = tf.abs(fg - probas[:, c])
         errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort_{}".format(c))
@@ -134,8 +137,10 @@ def lovasz_softmax_flat(probas, labels, only_present=False):
         losses.append(
             tf.tensordot(errors_sorted, tf.stop_gradient(grad), 1, name="loss_class_{}".format(c))
                       )
+    if len(class_to_sum) == 1:  # short-circuit mean when only one class
+        return losses[0]
     losses_tensor = tf.stack(losses)
-    if only_present:
+    if classes == 'present':
         present = tf.stack(present)
         losses_tensor = tf.boolean_mask(losses_tensor, present)
     loss = tf.reduce_mean(losses_tensor)
@@ -160,4 +165,3 @@ def flatten_probas(probas, labels, ignore=None, order='BHWC'):
     vprobas = tf.boolean_mask(probas, valid, name='valid_probas')
     vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
     return vprobas, vlabels
-
